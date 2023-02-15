35b0f5be2032eef8554849b7cffd8eac85bb4ecb
MD Shohag Mia <eshohag@outlook.com>
2023-02-15 21:38:39 +0600

    sdfgh

--- CRUD_NET7_Dapper_API/Helpers/AppDbContext.cs
+++ CRUD_NET7_Dapper_API/Helpers/AppDbContext.cs
@@ -33,34 +33,33 @@ namespace CRUD_NET7_Dapper_API.Helpers
         private async Task _initDatabase()
         {
             // create database if it doesn't exist
-            var connectionString = $"Server={_dbSettings.Server}; Database=master; User Id={_dbSettings.UserId}; Password={_dbSettings.Password};";
-            using var connection = new SqlConnection(connectionString);
-            var sql = $"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '{_dbSettings.Database}') CREATE DATABASE [{_dbSettings.Database}];";
-            await connection.ExecuteAsync(sql);
+            using (var connection = CreateConnection())
+            {
+                var sql = $"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = '{_dbSettings.Database}') CREATE DATABASE [{_dbSettings.Database}];";
+                await connection.ExecuteAsync(sql);
+            }
         }
 
         private async Task _initTables()
         {
             // create tables if they don't exist
-            using var connection = CreateConnection();
-            await _initUsers();
-
-            async Task _initUsers()
+            using (var connection = CreateConnection())
             {
                 var sql = """
-                    IF OBJECT_ID('Users', 'U') IS NULL
-                    CREATE TABLE Users (
-                        Id INT NOT NULL PRIMARY KEY IDENTITY,
-                        Title NVARCHAR(MAX),
-                        FirstName NVARCHAR(MAX),
-                        LastName NVARCHAR(MAX),
-                        Email NVARCHAR(MAX),
-                        Role INT,
-                        PasswordHash NVARCHAR(MAX)
-                    );
-                 """;
+                        IF OBJECT_ID('Users', 'U') IS NULL
+                        CREATE TABLE Users (
+                            Id INT NOT NULL PRIMARY KEY IDENTITY,
+                            Title NVARCHAR(MAX),
+                            FirstName NVARCHAR(MAX),
+                            LastName NVARCHAR(MAX),
+                            Email NVARCHAR(MAX),
+                            Role INT,
+                            PasswordHash NVARCHAR(MAX)
+                        );
+                     """;
                 await connection.ExecuteAsync(sql);
             }
+
         }
     }
 }
